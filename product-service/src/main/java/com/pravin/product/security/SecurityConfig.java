96ba2a2e5731ddf0e2f5e20f870b17efcd2db252
Ubuntu <ubuntu@ip-172-31-38-182.ap-south-1.compute.internal>
2026-07-10 07:24:18 +0000

    Add API Gateway and Docker Compose for Ecommerce Microservices

--- product-service/src/main/java/com/pravin/product/security/SecurityConfig.java
+++ product-service/src/main/java/com/pravin/product/security/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/actuator/**"
+                                "/actuator/**",
+				"/api/products/**"
                         ).permitAll()
 
                         .anyRequest().authenticated())
